b73ec9a0aebc141f577b3c41df219565c6c86660
Mohammad Aslam <mohammad.aslam@ucdconnect.ie>
2024-02-16 09:23:49 +0000

    Added and tested welcome method

--- BlackboxPlus.java
+++ BlackboxPlus.java
@@ -4,6 +4,9 @@
  */
 public class BlackboxPlus {
     //Welcome the user and take player information
+    public static void main(String[] args) {
+        String username = Util.printWelcome();
+    }
 
     //Create new Box with randomly generated atoms
 
--- Util.java
+++ Util.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Util {
+    public static String printWelcome(){
+        System.out.println("Welcome to BlackBox!!! :) :) :)");
+        System.out.println("Please enter user information:\t");
+        Scanner input = new Scanner(System.in);
+        String username = input.nextLine();
+        if(username.trim().isBlank()){
+            throw new IllegalArgumentException("WEONG USNERNAMEN");
+        }
+        else{
+            return username;
+        }
+    }
+
+
+
 }
