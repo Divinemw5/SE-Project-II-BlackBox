d7b9bf719a17eb1638b26f201fd71ffc6990b2e4
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2024-02-20 10:32:42 +0000

    Added function to display an empty board

--- Util.java
+++ Util.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Util {
+
+    static ArrayList<String> empty_board = new ArrayList<>(); //empty string rep. of board //initialize once
+
     public static String printWelcome(){
         System.out.println("Welcome to BlackBox!!! :) :) :)");
         System.out.println("Please enter user information:\t");
@@ -18,56 +23,77 @@ public class Util {
         printBoard();
     }
 
-    private static String printIndent(int x){
+    private static String duplicate(String str, int x) {
+        StringBuilder return_str = new StringBuilder();
+        for(int i = 0; i < x; i++) {
+            return_str.append(str);
+        }
+        return return_str.toString();
+    }
+
+    private static String getIndent(int x){
         StringBuilder return_val = new StringBuilder();
         for(int i = 0; i < x; i++){
             return_val.append("  ");
         }
         return return_val.toString();
     }
+
+    /**
+     * Function to print the board without atoms (for now)
+     */
     public static void printBoard(){
 
-        StringBuilder onea = new StringBuilder();
-        StringBuilder twoa = new StringBuilder();
-        StringBuilder threea = new StringBuilder();
+        //init string builders
+        String one = "    ██  "; //first line
+        String two = "  ██  ██"; //single spacing
+        String three="██      "; //triple spacing
 
-        String one = "    ██  ";
-        String two = "  ██  ██";
-        String three="██      ";
+        StringBuilder onea = new StringBuilder(duplicate(one,5)); //for appending lines
+        StringBuilder twoa = new StringBuilder(duplicate(two,5));
+        StringBuilder threea = new StringBuilder(duplicate(three,5));
 
+        int indentSize = 8; //initialize indent size
+        int appendLength = 0; //initialize appendLength
 
-        for(int i = 0; i <5; i++){
-            onea.append(one);
-            twoa.append(two);
-            threea.append(three);
-            if(i == 4){
-                threea.append(three.trim());
+        for(int i = 0; i<10; i++) {
+            //System.out.print(indentSize + ",");
+            //System.out.print(appendLength + ",");
+
+            //update indent (decrement for i < 5, increment for i > 5), update appendLength(opposite inc/dec)
+            if (i < 5) {
+                //store in arraylist (with indent)
+                empty_board.add(getIndent(indentSize) + onea + duplicate(one, appendLength));
+                empty_board.add(getIndent(indentSize) + twoa + duplicate(two, appendLength));
+                empty_board.add(getIndent(indentSize) + threea + duplicate(three, appendLength+1));
+                empty_board.add(getIndent(indentSize) + threea + duplicate(three, appendLength+1));
+                //update counter
+                indentSize -= 2;
+                appendLength++;
+            } else {
+                if(i == 5){
+                    appendLength--;
+                }
+                empty_board.add(getIndent(indentSize) + threea + duplicate(three, appendLength+1));
+                empty_board.add(getIndent(indentSize) + twoa + duplicate(two, appendLength));
+                if(i != 9) {
+                    empty_board.add(getIndent(indentSize) + onea + duplicate(one, appendLength));
+                    empty_board.add(getIndent(indentSize) + onea + duplicate(one, appendLength));
+                } else empty_board.add(getIndent(indentSize) + onea + duplicate(one, appendLength));
+                //update counter
+                if(i == 5){
+                    indentSize +=2;
+                }
+                indentSize += 2;
+                appendLength--;
             }
         }
+        System.out.println();
 
-        //for(int i = 0; i < )
-        System.out.println(printIndent(10) + onea.toString());
-        System.out.println(printIndent(10) + twoa.toString());
-        System.out.println(printIndent(10) + threea.toString());
-        System.out.println(printIndent(10) + threea.toString());
-        System.out.println(printIndent(10) + threea);
-
-
-
-/*
-        System.out.println("     ██      ██");
-        System.out.println("   ██  ██  ██  ██");
-        System.out.println(" ██      ██      ██");
-        System.out.println(" ██      ██      ██");
-        System.out.println(" ██      ██      ██");
-        System.out.println("   ██  ██  ██  ██");
-        System.out.println("     ██      ██");
-
- */
-
+        for(String str : empty_board){
+            System.out.println(str);
+        }
 
     }
-
-
 
 }
