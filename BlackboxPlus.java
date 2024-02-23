f58dcf22cb43806b26057003fc430db5e9af6301
Mohammad Aslam <mohammad.aslam@ucdconnect.ie>
2024-02-23 14:59:13 +0000

    implented program flow

--- BlackboxPlus.java
+++ BlackboxPlus.java
@@ -6,26 +6,32 @@ public class BlackboxPlus {
     //Welcome the user and take player information
 
     public static void main(String[] args) {
-        Atom[] atoms = Atom.generateAtoms(6);//generate random atoms
-        Box box = new Box(atoms);               //create the empty board
-        Util.printWelcome();                    //print welcome to user
 
-        //get username
-        while (true) {
-            try {
-                System.out.println("Enter a valid input:");
-                Player player1 = new Player(Util.lineInput());
-                // If input is valid, break out of the loop
-                break;
-            } catch (IllegalArgumentException e) {
-                // Handle invalid input by printing assigned error message from thrown exception
-                System.out.println(e.getMessage());
+        String userInput = "";
+        while(!userInput.equals("quit")) {
+
+
+            Player player1;
+            Atom[] atoms = Atom.generateAtoms(6);//generate random atoms
+            Box box = new Box(atoms);               //create the empty board
+            Util.printWelcome();                    //print welcome to user
+
+            //get username
+            while (true) {
+                try {
+                    player1 = new Player(Util.lineInput());
+                    // If input is valid, break out of the loop
+                    break;
+                } catch (IllegalArgumentException e) {
+                    // Handle invalid input by printing assigned error message from thrown exception
+                    System.out.println(e.getMessage());
+                }
             }
+
+            Util.printBoard(Util.getEmptyBoard());
+            System.out.println("WOULD YOU LIKE TO PLAY AGAIN OR NOT jhadsfKJHDFKSJDHFKJSHDFKJ");
+            userInput = Util.lineInput();
         }
-
-        Util.printBoard(Util.getEmptyBoard());
-
-
     }
 
     //Create new Box with randomly generated atoms
