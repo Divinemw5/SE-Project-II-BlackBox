323083e9a3594074dbc44df85242cabd5adea97b
Mohammad Aslam <mohammad.aslam@ucdconnect.ie>
2024-03-06 14:27:55 +0000

    finalising feature 2 test

--- TestFeature2Sprint2.java
+++ TestFeature2Sprint2.java
@@ -1,8 +1,48 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-
 import org.junit.Test;
 
-public class TestFeature1Sprint2 {
-    public void
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TestFeature2Sprint2 {
+    /**
+     * testing that each index that should be a sideHexagon is an instance of sideHexagon
+     */
+    @Test
+    public void testSideImplentation(){
+        Atom[] atoms  ={new Atom(4,4,4),new Atom(4,8,0),new Atom(4,0,8),new Atom(6,3,3),new Atom(3,6,3),new Atom(1,8,3)};
+        Box box = new Box(atoms);
+        Integer[] array = {0, 1, 2, 3, 4, 5, 10, 11, 17, 18, 25, 26, 34, 35, 42, 43, 49, 50, 55, 56, 57, 58, 59, 60};//store coordinates of all sideHexagons
+        List<Integer> SideHexagonIndices = Arrays.asList(array);
+        for(int i = 0; i < 60; i++){
+            if (SideHexagonIndices.contains(i)){
+                assertTrue("Hexagon at index "+ i +" should be a side hexagon", box.getHexagon(i) instanceof SideHexagon);
+            }
+            else{
+                assertFalse("Hexagon at index " + i + " should be a normal hexagon", box.getHexagon(i) instanceof SideHexagon);
+            }
+        }
+    }
+
+    @Test
+    public void testSpecificHexagonSideNumbers() {
+
+        Atom[] atoms  ={new Atom(4,4,4),new Atom(4,8,0),new Atom(4,0,8),new Atom(6,3,3),new Atom(3,6,3),new Atom(1,8,3)};
+        Box box = new Box(atoms);
+
+        // Test specific hexagons
+        assertHexagonSideNumbers(box, 0, List.of(/* expected side numbers */));
+        assertHexagonSideNumbers(box, 15, List.of(/* expected side numbers */));
+        // Add more tests for other specific hexagons
+    }
+
+    private void assertHexagonSideNumbers(Box box, int index, List<Integer> expectedSideNumbers) {
+        Hexagon hexagon = box.getHexagon(index);
+        assertTrue("Hexagon at index " + index + " should be an instance of SideHexagon", hexagon instanceof SideHexagon);
+        List<Integer> actualSideNumbers = ((SideHexagon) hexagon).getSideNumbers();
+        assertEquals("Side numbers do not match for hexagon at index " + index, expectedSideNumbers, actualSideNumbers);
+    }
+
+
 }
