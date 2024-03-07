82694435fbfef0dbc88bb037bdaebe2a9f8ba48d
Mohammad Aslam <mohammad.aslam@ucdconnect.ie>
2024-03-07 08:55:43 +0000

    Ray travel tests

--- TestRayTravelSprint2.java
+++ TestRayTravelSprint2.java
@@ -1,16 +1,36 @@
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.Assert.*;
 
-public class TestFeature3Sprint2 {
+public class TestRayTravelSprint2 {
+    private Box box;
+
+    @Before
+    public void setUp() {
+        Atom[] atoms = {
+                new Atom(-1,3,-2),
+                new Atom(0,1,-1),
+                new Atom(1,1,-1),
+                new Atom(4,-3,-1),
+                new Atom(-1,1,0),
+                new Atom(-2,2,0)
+        };
+        box = new Box(atoms);
+    }
+
     @Test
     public void testTravelAcross(){
-        Atom[] atoms  ={new Atom(-1,3,-2),new Atom(0,1,-1),new Atom(1,1,-1),new Atom(4,-3,-1),new Atom(-1,1,0),new Atom(-2,2,0)};
-        Box box = new Box(atoms);
         Ray ray = new Ray(2, box);
-        assertEquals(ray.getExit(), 45);
+        assertEquals("Expected Ray to exit at 45", 45, ray.getExit());
+
+        Ray ray1 = new Ray(16, box);
+        assertEquals("Expected Ray to exit at 31", 31, ray1.getExit());
     }
+
+    @Test
+    public void testAbsorbed(){
+        Ray ray = new Ray();
+
+    }
+
 }
