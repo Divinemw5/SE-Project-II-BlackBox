1c25def078f8ca4de9b8fceada940c07765f409f
Mohammad Aslam <mohammad.aslam@ucdconnect.ie>
2024-03-08 01:14:21 +0000

    beginning check for reflection announcement

--- Sprint2TestSuite.java
+++ Sprint2TestSuite.java
@@ -8,8 +8,8 @@ import org.junit.runners.Suite;
  * A feature that implements a ray that encounters no atom and travels across the board     DONE
  * A feature that implements a ray that makes a direct hit and the setter responds with “Absorbed” - [when the ray is travelling in the same direction as the atom and encounters it]   DONE
  * A feature that implements a ray that hits a hexagon with a barrier level of 1 and is deflected by 1 axis (60 degrees)    DONE
- * A feature that implements a ray that hits a hexagon with barrier level 1 before it enters the box and the setter announces “Reflected”
- * A feature that implements a ray that hits an atom directly on entering the board is the setter announces “Absorbed”
+ * A feature that implements a ray that hits a hexagon with barrier level 1 before it enters the box and the setter announces “Reflected” DONE
+ * A feature that implements a ray that hits an atom directly on entering the board is the setter announces “Absorbed” DONE
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
