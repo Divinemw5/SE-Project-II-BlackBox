fba26a1d8bdadd1f3518136a98472adc537b53da
Divinemw5 <divine.mwamba@ucdconnect.ie>
2024-04-05 00:31:28 +0100

    Test Of Ray Response and User Placement of Rays

--- Sprint3TestSuite.java
+++ Sprint3TestSuite.java
@@ -2,6 +2,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        Sprint3Tests.class,
+        BarrierLevelTest.class
 })
 public class Sprint3TestSuite {
 
